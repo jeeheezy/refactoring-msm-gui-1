4faa0be66f11c3bc03b1e37ae39f1fc8a41c640d
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-02-12 16:39:35 +0000

    Refactored with instance methods for returning database relations; rake grade 100%

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,4 +10,10 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
+  def movie
+    id = self.movie_id
+    matching_movies = Movie.where({ :id => id })
+    movie = matching_movies[0]
+    return movie
+  end
 end
